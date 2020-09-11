f8edf62be3298c9e51371a4991b442139b87b91a
Apoorv Yadav <apoorv2yadav@gmail.com>
2020-09-11 19:39:28 +0530

    Setup devDependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env) => {
     return {
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname,'public'),
+            path: path.join(__dirname,'public', 'dist'),
             filename: 'bundle.js'
         },
         module: {
@@ -34,7 +34,7 @@ module.exports = (env) => {
         devtool: isProduction ? 'source-map' : 'inline-source-map',
         devServer: {
          contentBase: path.join(__dirname, 'public'),
-         publicPath: '/public/',
+         publicPath: '/dist/',//let's us specify where those bundled assets should live
          historyApiFallback: true //this tells dev-server that we will be handling routing via client side code and it should return our main "/" page or all routes and let the code decide what to show(tells all pages/404 pages to send back the html file,video 4 folder 9) 
         }
      };
